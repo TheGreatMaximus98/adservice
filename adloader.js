b57b7a75d751d9f7128f201a2201f205b00d6e5d
TheGreatMaximus98 <155895542+crazycontent-adservice@users.noreply.github.com>
2024-01-07 09:54:57 -0500

    v1.0

---
+++ adloader.js
@@ -0,0 +1,17 @@
+const ADLIST = ["steph"]
+var ad = ADLIST[Math.floor(Math.random()*ADLIST.length)];
+var convertedJsonUrl = "ads/"+ad+".json"
+var convertedJson = null
+
+fetch(convertedJsonUrl)
+.then(res => res.json())
+.then(out => {
+    convertedJson = out
+    afterConverted()
+})
+
+function afterConverted(){
+    document.querySelector(".adimg").src = convertedJson.img
+    document.querySelector(".adurl").href = convertedJson.url
+    document.querySelector(".adreminder").style.display = "block"
+}
